c9230b8521b694a8df5f692e69cf68d8745d59f1
poornima2527 <poornii2527@gmail.com>
2026-09-01 20:53:47 +0530

    Connect login to Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,127 +1,86 @@
-function login(){
-
+function login() {
 
     let username = document.getElementById("username").value;
-
     let password = document.getElementById("password").value;
 
+    fetch("https://ecompulse-backend.onrender.com/api/auth/login", {
 
+        method: "POST",
 
-    fetch("http://localhost:5000/login",{
-
-
-        method:"POST",
-
-
-        headers:{
-
-
-            "Content-Type":"application/json"
-
-
+        headers: {
+            "Content-Type": "application/json"
         },
 
-
-        body:JSON.stringify({
-
-
-            username:username,
-
-            password:password
-
-
+        body: JSON.stringify({
+            username: username,
+            password: password
         })
 
+    })
+
+    .then(res => {
+
+        if (!res.ok) {
+            throw new Error("Login request failed");
+        }
+
+        return res.json();
 
     })
 
+    .then(data => {
 
-    .then(res=>res.json())
+        document.getElementById("message").innerHTML = data.message;
 
+        if (data.success) {
 
-    .then(data=>{
+            document.getElementById("message").style.color = "green";
 
+        } else {
 
-        document.getElementById("message").innerHTML=data.message;
-
-
-
-        if(data.success){
-
-
-            document.getElementById("message").style.color="green";
-
+            document.getElementById("message").style.color = "red";
 
         }
-
-        else{
-
-
-            document.getElementById("message").style.color="red";
-
-
-        }
-
 
     })
 
+    .catch(error => {
 
-    .catch(error=>{
+        console.error("Login error:", error);
 
+        document.getElementById("message").innerHTML =
+            "Unable to connect to server";
 
-        console.log(error);
-
+        document.getElementById("message").style.color = "red";
 
     });
 
-
 }
 
 
-
-
-
-// Show / Hide Password
-
+/* =========================
+   SHOW / HIDE PASSWORD
+========================= */
 
 let eye = document.getElementById("eye");
-
 let password = document.getElementById("password");
 
+eye.onclick = function () {
 
+    if (password.type === "password") {
 
-eye.onclick=function(){
-
-
-
-    if(password.type==="password"){
-
-
-        password.type="text";
-
+        password.type = "text";
 
         eye.classList.remove("fa-eye");
-
-
         eye.classList.add("fa-eye-slash");
 
+    } else {
 
-    }
-
-
-    else{
-
-
-        password.type="password";
-
+        password.type = "password";
 
         eye.classList.remove("fa-eye-slash");
-
-
         eye.classList.add("fa-eye");
-
 
     }
 
-
-}
+};
